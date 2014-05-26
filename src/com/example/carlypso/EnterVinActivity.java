2e63a44d68fa4f29822ae3e15238ce09ba73c2bf
bodhisattwa <das.bodhisattwa@gmail.com>
2014-05-26 19:13:48 +0530

    Initial Commit

---
+++ src/com/example/carlypso/EnterVinActivity.java
@@ -0,0 +1,50 @@
+package com.example.carlypso;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+
+public class EnterVinActivity extends Activity {
+	private ImageView imageViewBack, imageViewNext;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_enter_vin);
+		getIntent();
+		imageViewBack = (ImageView)findViewById(R.id.imageView1);
+		imageViewNext = (ImageView)findViewById(R.id.imageView2);
+		
+		//Assign onClick to back button
+		imageViewBack.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				startActivity(new Intent(getApplicationContext(),MainActivity.class));
+			}
+		});
+		//Assign onClick to back button
+		imageViewNext.setOnClickListener(new OnClickListener() {
+					
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				//startActivity(new Intent(getApplicationContext(),MainActivity.class));
+			}
+		});
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_enter_vin, menu);
+		return true;
+	}
+
+}
---
+++ src/com/example/carlypso/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.carlypso;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+
+public class MainActivity extends Activity {
+	private ImageView imageViewNext;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		getIntent();
+		imageViewNext = (ImageView)findViewById(R.id.imageView2);
+		imageViewNext.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				startActivity(new Intent(getApplicationContext(),EnterVinActivity.class));
+				
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+}
